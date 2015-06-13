b478e32760c5c3e054d816407c39277c0360a9eb
tonypai <tony77794@gmail.com>
2015-06-13 22:45:31 +0800

    Add Unitychan shoot attack

---
+++ Assets/Scripts/SelfDestruction.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelfDestruction : MonoBehaviour {
+
+	public float duration = 2f;
+
+	void Start () {
+		Destroy (gameObject, duration);
+	}
+}
---
+++ Assets/Scripts/UnitychanAttack.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitychanAttack : MonoBehaviour {
+
+	public float timeOffset = .5f;
+	public float shootForce = 200f;
+
+	[SerializeField] GameObject coin;
+	bool holdFire = false;
+
+	void Update () {
+		Debug.DrawRay(transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition), Color.green);
+
+		if (!holdFire && Input.GetButtonDown ("Fire1")) {
+			Vector3 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+			StartCoroutine ( Shoot ( direction ) );
+		}
+	}
+
+	IEnumerator Shoot (Vector3 target) {
+		holdFire = true;
+		GameObject bullet = (GameObject)Instantiate (
+			coin,
+			transform.position + transform.right,
+			Quaternion.identity
+		);
+		bullet.GetComponent<Rigidbody2D> ().AddForce (target * shootForce);
+		yield return new WaitForSeconds (timeOffset);
+		holdFire = false;
+	}
+}
