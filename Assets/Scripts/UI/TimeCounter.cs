f73f4d9e5743dc1158bacef0e2ea69058e123414
tonypai <tony77794@gmail.com>
2015-07-01 23:51:42 +0800

    Add time counter

---
+++ Assets/Scripts/UI/TimeCounter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TimeCounter : MonoBehaviour {
+	
+	public Text timeText;
+
+	private bool m_stop = false;
+	public bool Stop { set { m_stop = value; } }
+	private float m_nowTime = 200f;
+	
+	private static TimeCounter m_instance;
+	public static TimeCounter instance
+	{
+		get
+		{
+			if (m_instance == false)
+			{
+				m_instance = FindObjectOfType<TimeCounter>();
+			}
+			return m_instance;
+		}
+	}
+
+	void FixedUpdate () {
+		if (!m_stop) {
+			m_nowTime -= Time.deltaTime;
+			timeText.text = m_nowTime.ToString ("000");
+		}
+	}
+}
